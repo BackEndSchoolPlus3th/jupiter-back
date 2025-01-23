443e524a091b2806a7c47905a735310529262c38
dev_gyuil <120631088+System-out-gyuil@users.noreply.github.com>
2025-01-23 16:34:37 +0900

    Revert "test : 서비스 실행 및 컨트롤러를 통해 실행 확인(#5)"

--- src/main/java/com/jupiter/wyl/domain/movie/movie/controller/MovieController.java
+++ src/main/java/com/jupiter/wyl/domain/movie/movie/controller/MovieController.java
@@ -1,18 +1,4 @@
 package com.jupiter.wyl.domain.movie.movie.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
 public class MovieController {
-
-    @GetMapping("/test")
-    @ResponseBody
-    public String controllerTest() {
-        return "test";
-    }
-
 }
