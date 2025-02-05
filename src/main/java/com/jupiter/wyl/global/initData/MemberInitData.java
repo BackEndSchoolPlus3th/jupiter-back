3287f3684801f6d72efa4beb8a0286fa2e917a9b
whale22 <realwhale64@naver.com>
2025-02-05 15:00:09 +0900

    feat: 멤버 엔티티 수정 및 가데이터 추가

--- src/main/java/com/jupiter/wyl/global/initData/MemberInitData.java
+++ src/main/java/com/jupiter/wyl/global/initData/MemberInitData.java
@@ -21,9 +21,19 @@ public class MemberInitData {
             @Transactional
             @Override
             public void run(ApplicationArguments args) throws Exception {
-//                Member member1 = memberService.join("apple@aaa.aaa", "김애플", "1234");
-//                Member member2 = memberService.join("banana@aaa.aaa", "이바나나", "1234");
-//                Member member3 = memberService.join("cherry@aaa.aaa", "최체리", "1234");
+                Member member1 = memberService.join("apple@aaa.aaa", "김호러", "1234");
+                Member member2 = memberService.join("banana@aaa.aaa", "이사랑", "1234");
+                Member member3 = memberService.join("cherry@aaa.aaa", "최웃음", "1234");
+
+                //가데이터 입력
+                member1.setLikeGenres("공포,스릴러,로맨스");
+                member1.setLikeKeywords("없음");
+
+                member1.setLikeGenres("로맨스,드라마,모험,코미디");
+                member1.setLikeKeywords("없음");
+
+                member1.setLikeGenres("액션,가족,애니메이션");
+                member1.setLikeKeywords("없음");
             }
         };
     }
