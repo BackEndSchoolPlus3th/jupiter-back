e0e2d134dfc866020bd02071ebdc2f31e41b7bbc
seoyeonson <eo970721@gmail.com>
2025-02-08 22:49:13 +0900

    fix : 회원 정보 중복 저장 오류 수정

--- src/main/java/com/jupiter/wyl/global/initData/MemberInitData.java
+++ src/main/java/com/jupiter/wyl/global/initData/MemberInitData.java
@@ -2,7 +2,6 @@ package com.jupiter.wyl.global.initData;
 
 
 import com.jupiter.wyl.domain.member.entity.Member;
-import com.jupiter.wyl.domain.member.repository.MemberRepository;
 import com.jupiter.wyl.domain.member.service.MemberService;
 import jakarta.transaction.Transactional;
 import org.springframework.boot.ApplicationArguments;
@@ -16,18 +15,28 @@ import org.springframework.context.annotation.Profile;
 public class MemberInitData {
     @Bean
     public ApplicationRunner applicationRunner(
-            MemberService memberService,
-            MemberRepository memberRepository
+            MemberService memberService
     ) {
         return new ApplicationRunner() {
             @Transactional
             @Override
             public void run(ApplicationArguments args) throws Exception {
-//                Member member1 = memberService.join("apple@aaa.aaa", "김호러", "123456");
-//                Member member2 = memberService.join("banana@aaa.aaa", "이사랑", "123456");
-//                Member member3 = memberService.join("cherry@aaa.aaa", "최웃음", "123456");
 
-                //가데이터 입력 부분 join 함수 내에서 처리
+                if (memberService.findByEmail("apple@aaa.aaa") == null) {
+                    Member member1 = memberService.join("apple@aaa.aaa", "김호러", "123456");
+                    Member member2 = memberService.join("banana@aaa.aaa", "이사랑", "123456");
+                    Member member3 = memberService.join("cherry@aaa.aaa", "최웃음", "123456");
+
+                    //가데이터 입력
+                    member1.setLikeGenres("공포,스릴러,로맨스");
+                    member1.setLikeKeywords("없음");
+
+                    member1.setLikeGenres("로맨스,드라마,모험,코미디");
+                    member1.setLikeKeywords("없음");
+
+                    member1.setLikeGenres("액션,가족,애니메이션");
+                    member1.setLikeKeywords("없음");
+                }
             }
         };
     }
