27984a00e02f014504d9a01ac5117ecaa900e440
seoyeonson <eo970721@gmail.com>
2025-02-08 22:49:13 +0900

    fix : 회원 정보 중복 저장 오류 수정

--- src/main/java/com/jupiter/wyl/global/initData/MemberInitData.java
+++ src/main/java/com/jupiter/wyl/global/initData/MemberInitData.java
@@ -21,19 +21,22 @@ public class MemberInitData {
             @Transactional
             @Override
             public void run(ApplicationArguments args) throws Exception {
-                Member member1 = memberService.join("apple@aaa.aaa", "김호러", "123456");
-                Member member2 = memberService.join("banana@aaa.aaa", "이사랑", "123456");
-                Member member3 = memberService.join("cherry@aaa.aaa", "최웃음", "123456");
 
-                //가데이터 입력
-                member1.setLikeGenres("공포,스릴러,로맨스");
-                member1.setLikeKeywords("없음");
+                if (memberService.findByEmail("apple@aaa.aaa") == null) {
+                    Member member1 = memberService.join("apple@aaa.aaa", "김호러", "123456");
+                    Member member2 = memberService.join("banana@aaa.aaa", "이사랑", "123456");
+                    Member member3 = memberService.join("cherry@aaa.aaa", "최웃음", "123456");
 
-                member1.setLikeGenres("로맨스,드라마,모험,코미디");
-                member1.setLikeKeywords("없음");
+                    //가데이터 입력
+                    member1.setLikeGenres("공포,스릴러,로맨스");
+                    member1.setLikeKeywords("없음");
 
-                member1.setLikeGenres("액션,가족,애니메이션");
-                member1.setLikeKeywords("없음");
+                    member1.setLikeGenres("로맨스,드라마,모험,코미디");
+                    member1.setLikeKeywords("없음");
+
+                    member1.setLikeGenres("액션,가족,애니메이션");
+                    member1.setLikeKeywords("없음");
+                }
             }
         };
     }
