607c2fe2c44001bb4af272ea4e7f3911e04d489a
System-out-gyuil <a46884334@gmail.com>
2025-02-10 09:44:04 +0900

    feat : 영화 상세 작업 (#5)

--- src/main/java/com/jupiter/wyl/domain/movie/movie/controller/MovieController.java
+++ src/main/java/com/jupiter/wyl/domain/movie/movie/controller/MovieController.java
@@ -6,8 +6,6 @@ import com.jupiter.wyl.domain.movie.movie.dto.request.ReviewRequest;
 import com.jupiter.wyl.domain.movie.movie.dto.response.MovieDto;
 import com.jupiter.wyl.domain.movie.movie.dto.response.MovieReviewDto;
 import com.jupiter.wyl.domain.movie.movie.dto.response.MovieSearchDto;
-import com.jupiter.wyl.domain.movie.movie.entity.Movie;
-import com.jupiter.wyl.domain.movie.movie.entity.MovieReview;
 import com.jupiter.wyl.domain.movie.movie.service.MovieSearchService;
 import com.jupiter.wyl.domain.movie.movie.service.MovieService;
 import com.jupiter.wyl.domain.movie.movie.service.MovieReviewService;
@@ -56,10 +54,6 @@ public class MovieController {
 
     @PostMapping("/review/write")
     public String receiveReview(@RequestBody ReviewRequest reviewRequest) {
-        System.out.println("Received review: " + reviewRequest.getReviewContent());
-        System.out.println("Received id: " + reviewRequest.getMovie());
-        System.out.println("Received userId: " + reviewRequest.getUserId());
-        System.out.println("Received rating: " + reviewRequest.getRating());
 
         String reviewContent = reviewRequest.getReviewContent();
         int rating = reviewRequest.getRating();
@@ -68,6 +62,6 @@ public class MovieController {
 
         movieReviewService.saveReview(userId, reviewContent, rating, movie);
 
-        return "리뷰가 성공적으로 저장되었습니다!";
+        return "-----------------------review-write-success----------------------------";
     }
 }
